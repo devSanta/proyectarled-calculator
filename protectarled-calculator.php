98ee7acfc37f544253e0536395801fb0007d8355
Cristian <clhenaos@unal.edu.co>
2019-09-08 10:14:03 -0500

    change plugin URI

--- protectarled-calculator.php
+++ protectarled-calculator.php
@@ -1,8 +1,9 @@
 /*
 Plugin Name: Led Calculator
-Plugin URI: http://localhost/
+Plugin URI: https://github.com/devSanta/proyectarled-calculator
 Description: Plugin by proyectarLed to make suggestions about their led products.
 Author: Cristian Leandro Henao
 Version:1.0.0
 Author URI: https://github.com/devSanta
 */
+
