be83b32d1044b07856334f3dbaa35c0660249e78
Cristian Leandro Henao Santa <clhenaos@unal.edu.co>
2019-09-08 08:41:42 -0500

    Initial pluging config

---
+++ protectarled-calculator.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: Led Calculator
+Plugin URI: http://localhost/
+Description: Plugin by proyectarLed to make suggestions about their led products.
+Author: Cristian Leandro Henao
+Version:1.0.0
+Author URI: https://github.com/devSanta
+*/
